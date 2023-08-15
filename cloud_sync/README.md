fc62144af164c1eee9d98bb4456d6dbc2eae6ad8
Sergii Galai <sergiy.galay@gmail.com>
2023-08-15 15:12:32 +0200

    [gdrive] support dropbox configuration

--- cloud_sync/README.md
+++ cloud_sync/README.md
@@ -1,9 +1,9 @@
-Sync local directory to Google Drive
+Sync local directory to Google Drive or Dropbox
 
-Problem
+## Problem
 Normally we install the Google Drive client on our system which takes care of all the syncing. So you can keep editing your files locally inside the Google Drive Folder without worrying about them getting synced with your google account because the client will take care of it. However, due to certain restrictions on my system the Google Drive client cannot be installed. 
 
-What it can:
+### What it can:
 - synchronize files in your local folder and folder on dropbox account per script run
 - track files inside the directory
 - track subfolders and their content inside the directory
@@ -12,42 +12,58 @@ What it can:
 - open source command line tool so your files belong to you (and Dropbox :) )
 - no need to install (except python runtime and dependency module)
 
-What it cannot:
+### What it cannot:
 - cannot live in memory (not a realtime client)
 - cannot track your file changes in realtime
 - cannot track your dropbox changes in realtime
 - cannot handle file *removals*. You need to check that manually. So if you have removed file on dropbox but preserved in local folder this tool will propose to upload file again (and vise versa).
 
-To start working:
-1. You have to have a Dropbox account
-1. Copy `cloud_sync_client.py` to your local disk
+## Start working
+1. Copy the content of this folder to your local disk
 1. Create `config.ini` or other file. Check its content below.
 1. Install python3. E.g. using Chocolatey `choco install -y python`
+
+### Google Drive
+1. You have to have a Dropbox account
 1. Install python dropbox dependency module `pip3 install dropbox`
 1. Create app on dropbox website. Read more https://docs.gravityforms.com/creating-a-custom-dropbox-app/
 1. Open created app https://www.dropbox.com/developers/apps
 1. Scroll down to ‘OAuth 2’ block and hit `Generate` button near `Generated access token` text
-1. After the token is generated copy it to `DROPBOX_TOKEN` field in `config.ini`
-1. run `python d:\path\dropbox_client.py`. If your config has other name then `python d:\path\dropbox_client.py -c d:\path\to\config\other_name.ini`
+1. After the token is generated copy it to `TOKEN` field in `config.ini`
+1. run `python d:\path\cloud_sync.py`
+
+### Dropbox
+1. You have to have a Dropbox account
+1. Install python dropbox dependency module `pip3 install dropbox`
+1. Create app on dropbox website. Read more https://docs.gravityforms.com/creating-a-custom-dropbox-app/
+1. Open created app https://www.dropbox.com/developers/apps
+1. Scroll down to ‘OAuth 2’ block and hit `Generate` button near `Generated access token` text
+1. After the token is generated copy it to `TOKEN` field in `config.ini`
+1. run `python d:\path\cloud_sync.py`
+
+### Examples
+`python d:\path\cloud_sync.py -h` - get help
+`python d:\path\cloud_sync.py`
+`python d:\path\cloud_sync.py -c d:\path\to\config\other_name.ini`
+
+### Config file
 
 `config.ini` content:
 ```
-[DBOX_SYNC]
-DROPBOX_TOKEN = [token_recieved_on_dropbox_com]
-LOCAL_DIR = [path to database root folder (absolute or relative)]
-DBOX_DIR = [path to folder on dropbox]
-DRY_RUN = [False|True]
-SUBFOLDERS = [if true process also subfolders. Allowed values: False|True]
-ACTION = [sync|download|upload]
-```
+STORAGE = [DROPBOX|GDRIVE]
 
-Config file Example:
-```
-[DBOX_SYNC]
-DROPBOX_TOKEN = 1234567890
-LOCAL_DIR = ./db
-DBOX_DIR = /documents
-DRY_RUN = False
-SUBFOLDERS = True
-ACTION = sync
+[GDRIVE]
+ACTION = [sync|download|upload]
+LOCAL_DIR = [path to database root folder (absolute or relative)]
+CLOUD_DIR = [path to folder on Goodle Drive]
+RECURSIVE = [False|True - process subfolders if true]
+DRY_RUN = [False|True]
+
+[DROPBOX]
+ACTION = [sync|download|upload]
+TOKEN = [token_recieved_on_dropbox_com. see https://www.dropbox.com/developers/apps]
+LOCAL_DIR = [path to database root folder (absolute or relative)]
+CLOUD_DIR = [path to folder on Dropbox]
+RECURSIVE = [False|True - process subfolders if true]
+DRY_RUN = [False|True]
 ```
--- cloud_sync/cloud_sync.py
+++ cloud_sync/cloud_sync.py
@@ -28,9 +28,9 @@ class Config:
     action: str
     token: str
     local_dir: str
-    dbox_dir: str
+    cloud_dir: str
     dry_run: bool
-    sub_folders: bool
+    recursive: bool
 
 class ConfigProvider:
     def __get_config_file_location(self, path: str):
@@ -43,39 +43,40 @@ class ConfigProvider:
                 os.path.join(os.path.expanduser('~'), '.cloud_sync_config.ini')
             ]
 
+    def __validate_booleans(self, args):
+        if sum([bool(b) for b in (args.yes, args.no, args.default)]) > 1:
+            logger.error('At most one of --yes, --no, --default is allowed')
+            sys.exit(2)
+
     def __parse_arguments(self):
         logger.debug('parse_arguments started')
         parser = argparse.ArgumentParser()
         parser.add_argument('-c', '--config', help='Config file')
-        parser.add_argument('--source', help='Local directory to upload')
-        parser.add_argument('--dest', help='Destination folder on your cloud storage')
+        parser.add_argument('--action', help='download|upload|sync files (sync if not exist)')
         parser.add_argument('--token', help='Access token')
+        parser.add_argument('--local_dir', help='Local directory to upload')
+        parser.add_argument('--cloud_dir', help='Destination folder on your cloud storage')
         parser.add_argument('--yes', '-y', action='store_true', help='Answer yes to all questions')
         parser.add_argument('--no', '-n', action='store_true', help='Answer no to all questions')
         parser.add_argument('--default', '-d', action='store_true', help='Take default answer on all questions')
-        parser.add_argument('--action', help='download|upload|sync files (sync if not exist)')
         parser.add_argument('--dryrun', help='Do not change files if True')
-        parser.add_argument('--subfolders', help='Do not change files if True')
+        parser.add_argument('--recursive', help='Process subfolders if True')
         return parser.parse_args()
 
     def __get_from_args_or_config(self, args, config):
         logger.debug('__get_from_args_or_config started. args={}, config={}'.format(args, config))
         action = args.action or config.get('ACTION')
-        dbox_dir = args.dest or config.get('CLOUD_DIR') or ""
+        token = args.token or config.get('TOKEN')
+        local_dir = args.local_dir or config.get('LOCAL_DIR') or ""
+        cloud_dir = args.cloud_dir or config.get('CLOUD_DIR') or ""
+        dry_run = args.dryrun or config.getboolean('DRY_RUN')
+        recursive = args.recursive or config.getboolean('RECURSIVE')
 
-        local_dir = args.source or config.get('LOCAL_DIR') or ""
+        self.__validate_booleans(args)
         if local_dir.startswith('.'):
             local_dir = os.path.abspath(local_dir)
 
-        dry_run = args.dryrun or config.getboolean('DRY_RUN')
-        sub_folders = args.subfolders or config.getboolean('SUBFOLDERS')
-
-        if sum([bool(b) for b in (args.yes, args.no, args.default)]) > 1:
-            logger.error('At most one of --yes, --no, --default is allowed')
-            sys.exit(2)
-
-        token = None
-        return Config(action, token, local_dir, dbox_dir, dry_run, sub_folders)
+        return Config(action, token, local_dir, cloud_dir, dry_run, recursive)
 
     def get_config(self):
         logger.debug('get_config started')
@@ -85,10 +86,19 @@ class ConfigProvider:
         config = configparser.ConfigParser()
         config.read( config_location )
 
-        if not config.has_section('GDRIVE'): config['GDRIVE'] = {}
-        gdriveConfig = config['GDRIVE']
+        if not config.has_section('GENERAL'): config['GENERAL'] = {}
 
-        return self.__get_from_args_or_config(args, gdriveConfig)
+        match config['GENERAL'].get('STORAGE'):
+            case 'GDRIVE': storage = 'GDRIVE'
+            case 'DROPBOX': storage = 'DROPBOX'
+            case _:
+                print('Not supported STORAGE value')
+                sys.exit(2)
+
+        if not config.has_section(storage): config[storage] = {}
+        storageConfig = config[storage]
+
+        return self.__get_from_args_or_config(args, storageConfig)
 
 @dataclass
 class FileMetadata:
@@ -217,14 +227,14 @@ class FileMapper:
     def __init__(self, fileStore: FileStore, dboxStore: DropboxStore, conf: Config):
         self.fileStore = fileStore
         self.dboxStore = dboxStore
-        self.subfolders = conf.sub_folders
+        self.recursive = conf.recursive
 
     def map_recursive(self, dbox_path: str):
         logger.info('dbox_path={}'.format(dbox_path))
         local_root, local_dirs, local_files = self.fileStore.list_folder(dbox_path)
         dbx_root, dbx_dirs, dbx_files = self.dboxStore.list_folder(dbox_path)
         download_files, upload_files = self.__map_dropbox_files_to_local(local_root, local_files, dbx_root, dbx_files)
-        if self.subfolders:
+        if self.recursive:
             process_folders = self.__map_dropbox_folders_to_local(local_dirs, dbx_root, dbx_dirs)
             for dbox_folder in process_folders:
                 download_sub, upload_sub = self.map_recursive(dbox_folder)
@@ -424,7 +434,7 @@ if __name__ == '__main__':
     #fileMapper = FileMapper(fileStore, dboxStore, conf)
     #ui = UI()
     #controller = Controller(fileStore, dboxStore, fileMapper, ui)
-    #if conf.action == 'download': controller.sync(conf.dbox_dir, download=True, upload=False)
-    #elif conf.action == 'upload': controller.sync(conf.dbox_dir, download=False, upload=True)
-    #elif conf.action == 'sync':   controller.sync(conf.dbox_dir, download=True, upload=True)
+    #if conf.action == 'download': controller.sync(conf.cloud_dir, download=True, upload=False)
+    #elif conf.action == 'upload': controller.sync(conf.cloud_dir, download=False, upload=True)
+    #elif conf.action == 'sync':   controller.sync(conf.cloud_dir, download=True, upload=True)
     #else: logger.error('Unknown action in configuration')
